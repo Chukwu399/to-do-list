6160f9e2ea696a2e11381dcb2e9adad8b8ea5a1b
Chukwu399 <chukwumichael39@gmail.com>
2023-07-31 18:28:26 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+
+function App() {
+  
+  const [items, setItems] = useState([]);
+
+  
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText]
+    });
+    
+  } 
+
+  
+
+ function deletedItem(id) {
+   setItems(prevItems => {
+    return prevItems.filter((item, index) => {
+      return index !== id;
+    })
+   })
+ }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea
+        onAdd={addItem}
+      />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+           <ToDoItem
+           key={index} 
+           text= {todoItem}
+           id={index}
+           onChecked={deletedItem}
+           
+           />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+
+function ToDoItem({text, onChecked, id}) {
+    
+
+
+    
+
+
+    return (
+        <div onClick={()=> { onChecked(id)}} >
+        <li>{text}</li>
+        </div>
+    ) 
+}
+
+export default ToDoItem;
